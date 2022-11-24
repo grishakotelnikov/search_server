ea68bdedd5b1f659cdde792093c4efc279a9f8b8
grishakotelnikov <106670234+grishakotelnikov@users.noreply.github.com>
2022-11-24 17:20:16 +0300

    Update remove_duplicates.cpp

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,1 +1,28 @@
 
+
+#include "remove_duplicates.h"
+
+void RemoveDuplicates(SearchServer& search_server) {
+    std::vector<int> ids_to_remove;
+    std::set<std::set<std::string>> unique_documents_words;
+
+    for (const int document_id : search_server) {
+        std::set<std::string> words_in_document;
+        for (const auto& [word, _] : search_server.GetWordFrequencies(document_id)) {
+            words_in_document.insert(word);
+        }
+        if (unique_documents_words.count(words_in_document))
+        {
+            ids_to_remove.push_back(document_id);
+        }
+        else {
+            unique_documents_words.insert(words_in_document);
+        }
+    }
+
+    for (const int document_id : ids_to_remove) {
+        std::cout << "Found duplicate document id " << document_id << std::endl;
+        search_server.RemoveDocument(document_id);
+    }
+}
+
